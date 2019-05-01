f28068a28f6c2e303493fcfb5ebdd7831b26e4ce
tnjohnson <tjohnson27@gmail.com>
2019-04-30 20:16:26 -0600

    added addtional number

--- tim.js
+++ tim.js
@@ -1,2 +1,9 @@
 console.log("Hi, My name is Tim.")
 console.log("This is Javascript!!! (Kicks guy into deep pit)")
+
+const stuff = 7+ 7
+console.log(stuff)
+
+const number = 8
+
+console.log(number)
